1554cbca5eb1faf02f3c16af2d2f0d8e8c573deb
Dal4Segno <dal4segno@gmail.com>
2017-07-06 18:04:40 +0900

    ThreadManager와 Iocp 클래스의 기본 구조 정의

--- core/thread_manager/ThreadManager.h
+++ core/thread_manager/ThreadManager.h
@@ -1,14 +1,25 @@
 #pragma once
 #include <Windows.h>
+#include <unordered_set>
+#include "Iocp.h"
 
 namespace core 
 {
-    class ThreadManager
-    {
-    public:
-        ThreadManager();
-        ~ThreadManager();
-    private:
-        int current_thread_count;
-    };
+class ThreadManager
+{
+public:
+    ThreadManager();
+    ~ThreadManager();
+    
+    BOOL set_maximum_thread_count(int);
+    int get_usable_thread_count(int);
+
+    BOOL add_iocp(Iocp iocp);
+    BOOL remove_iocp(Iocp iocp);
+    BOOL pause_iocp(Iocp iocp);
+private:
+    int maximum_thread_count = 0;
+    int current_thread_count = 0;
+    std::unordered_set<Iocp> iocp_set;
+};
 }
