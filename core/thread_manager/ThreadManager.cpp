c25efbf1bc05f6306b677770138c791fbf38dd2e
Dal4Segno <dal4segno@gmail.com>
2017-07-10 15:58:55 +0900

    Iocp 클래스를 ThreadManager 내의 작은 구조체로 변경하여 구조를 간소화 함 각 IOCP를 char * 형의 이름으로 구분하여 관리하도록 변경

--- core/thread_manager/ThreadManager.cpp
+++ core/thread_manager/ThreadManager.cpp
@@ -1,12 +1,64 @@
 #include "ThreadManager.h"
+
 namespace core
 {
+
 ThreadManager::ThreadManager()
 {
+    GetSystemInfo(&system_info);
+    maximum_thread_count = system_info.dwNumberOfProcessors * 2 + 2;
 }
-
 
 ThreadManager::~ThreadManager()
 {
 }
+
+BOOL ThreadManager::set_maximum_thread_count(int)
+{
+    return 0;
+}
+
+int ThreadManager::get_usable_thread_count()
+{
+    return maximum_thread_count - current_thread_count;
+}
+
+ThreadManager::Iocp ThreadManager::create_iocp(char * name, int concurrent, unsigned (*function)(LPVOID))
+{
+    Iocp new_iocp;
+    int real_thread_pool_size = concurrent * 2;
+    if (current_thread_count + real_thread_pool_size > maximum_thread_count)
+        new_iocp.iocp = NULL;
+    else
+    {
+        current_thread_count += real_thread_pool_size;
+        new_iocp.iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, concurrent);
+        new_iocp.tp.resize(real_thread_pool_size);
+        for (HANDLE th : new_iocp.tp)
+            th = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)function, new_iocp.iocp, 0, NULL);
+        iocps.insert({ name, new_iocp });
+    }
+    return new_iocp;
+}
+
+BOOL ThreadManager::associate_iocp(char * name, HANDLE device, DWORD key)
+{
+    return CreateIoCompletionPort(device, iocps[name].iocp, key, 0) == iocps[name].iocp;
+}
+
+BOOL ThreadManager::remove_iocp(char * name)
+{
+    return iocps.erase(name) == 1;
+}
+
+BOOL ThreadManager::pause_iocp(char * name)
+{
+    return 0;
+}
+
+BOOL ThreadManager::resume_iocp(char * name)
+{
+    return 0;
+}
+
 }
--- core/thread_manager/ThreadManager.h
+++ core/thread_manager/ThreadManager.h
@@ -1,25 +1,36 @@
 #pragma once
 #include <Windows.h>
-#include <unordered_set>
-#include "Iocp.h"
+#include <process.h>
+#include <unordered_map>
+#include <vector>
 
 namespace core 
 {
 class ThreadManager
 {
 public:
+    typedef struct _Iocp
+    {
+        HANDLE iocp;
+        std::vector<HANDLE> tp;
+    }Iocp;
+
     ThreadManager();
     ~ThreadManager();
     
     BOOL set_maximum_thread_count(int);
-    int get_usable_thread_count(int);
+    int get_usable_thread_count();
 
-    BOOL add_iocp(Iocp iocp);
-    BOOL remove_iocp(Iocp iocp);
-    BOOL pause_iocp(Iocp iocp);
+    ThreadManager::Iocp create_iocp(char * name, int concurrent, unsigned (*function)(LPVOID));
+    BOOL associate_iocp(char * name, HANDLE device, DWORD key);
+    BOOL remove_iocp(char * name);
+    BOOL pause_iocp(char * name);
+    BOOL resume_iocp(char * name);
+
 private:
     int maximum_thread_count = 0;
     int current_thread_count = 0;
-    std::unordered_set<Iocp> iocp_set;
+    std::unordered_map<char *, Iocp> iocps;
+    SYSTEM_INFO system_info;
 };
 }
