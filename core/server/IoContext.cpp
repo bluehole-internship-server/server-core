22bb40f53b0b7b148bd3fd92052dfa5082e34363
Dal4Segno <dal4segno@gmail.com>
2017-07-24 20:08:34 +0900

    IoContext 종류 추가 및 Buffer 추가

--- core/server/IoContext.cpp
+++ core/server/IoContext.cpp
@@ -2,8 +2,18 @@
 
 IoContext::IoContext()
 {
-	SecureZeroMemory(&overlapped, sizeof(overlapped));
-	SecureZeroMemory(&buffer, sizeof(buffer));
-	buffer.len = 0;
-	buffer.buf = nullptr;
+	SecureZeroMemory(&overlapped_, sizeof(overlapped_));
+	SecureZeroMemory(&buffer_, sizeof(buffer_));
+	buffer_.len = 0;
+	buffer_.buf = nullptr;
+	send_buffer_ = new char[SEND_BUFFER_SIZE];
+	recv_buffer_ = new char[RECV_BUFFER_SIZE];
+	SecureZeroMemory(send_buffer_, sizeof(send_buffer_));
+	SecureZeroMemory(recv_buffer_, sizeof(recv_buffer_));
+}
+
+IoContext::~IoContext()
+{
+	delete send_buffer_;
+	delete recv_buffer_;
 }
--- core/server/IoContext.h
+++ core/server/IoContext.h
@@ -1,20 +1,29 @@
 #pragma once
 #include <WinSock2.h>
+#include "Client.h"
+
+#define SEND_BUFFER_SIZE 32
+#define RECV_BUFFER_SIZE 256
 
 enum IoType
 {
 	IO_NONE,
 	IO_ACCEPT,
+	IO_RECV_READY,
 	IO_RECV,
-	IO_CONNECT
+	IO_CONNECT,
+	IO_DISCONNECT
 };
 
 struct IoContext
 {
 	IoContext();
+	~IoContext();
 
-	OVERLAPPED overlapped;
-	SOCKET socket;
-	WSABUF buffer;
-	IoType io_type;
+	OVERLAPPED overlapped_;
+	Client client_;
+	WSABUF buffer_;
+	IoType io_type_;
+	char * send_buffer_;
+	char * recv_buffer_;
 };
